e55b92b33ad60d2e32b0ce0ec1f26406511cae92
Matej Drenjančević <matej.drenjancevic@gmail.com>
2024-05-15 17:09:14 +0200

    Fixed decode Array

--- CbPage-backend/Encoders/Decoder.cs
+++ CbPage-backend/Encoders/Decoder.cs
@@ -6,12 +6,14 @@ namespace CbPage_backend.Encoders
 {
     internal class Decoder
     {
-        public static string DecodeArray(byte[] input)
+        public static byte[] DecodeArray(byte[] input)
         {
             int lenth = BitConverter.ToInt16(input, 0);
-            byte[] stringu8 = new byte[lenth];
-            Array.Copy(input, 2, stringu8, 0, lenth);
-            return Encoding.UTF8.GetString(stringu8);
+
+            byte[] bytes = new byte[lenth];
+            Array.Copy(input, 2, bytes, 0, lenth);
+            return bytes;
+            
         }
 
         public static string DecodeString(byte[] input)
