b1fa08963bbc313113e50a7f9a83e8d7debc2c48
Matej Drenjančević <matej.drenjancevic@gmail.com>
2024-05-15 15:53:00 +0200

    Fixed mistakes in upgrading

--- CbPage-backend/Program.cs
+++ CbPage-backend/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CbPage_backend
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
